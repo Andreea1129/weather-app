9c7eda7da404c49c8ca5b6e6d4fc945044557235
Andreea1129 <vilcuandreea12@gmail.com>
2026-02-19 15:33:22 +0200

    updated utils

--- utils/date.js
+++ utils/date.js
@@ -1,13 +1,11 @@
-// Ne definim 2 functii utilitare care o sa ne extraga dintr-o data in format UTC: ziua si ora
+// Definesc functii utilitare care o sa ne extraga dintr-o data in format UTC: ziua si ora:minutele
+
 function getDayOfTheWeek (dateInUtcFormat){
-    // Pentru a putea crea o data pornind de la o valoare in UTC  este nevoie sa o inmultim cu 1000
     const date = new Date(dateInUtcFormat * 1000);
-    // Extragem ziua saptamanii sub forma de index
     const dayIndex=date.getDay();
-    // console.log(dayIndex);
+    
     let day;
 
-    // Pentru a mapa index-ul zilei cu zilele in romana o sa folosim un switch
     switch(dayIndex){
         case 0:
             day = "Duminică";
@@ -31,31 +29,23 @@ function getDayOfTheWeek (dateInUtcFormat){
             day="Sâmbătă";
             break;
         default:
-            // Aruncam o eroare daca index-ul nu este valid
             throw new Error('Indexul trebuie sa fie intre 0 si 6');
     }
-    // console.log(day);
     return day;
 }
 
 function getHour(dateInUtcFormat){
     const date = new Date (dateInUtcFormat*1000);
-    // extragem ora
     let hour = date.getHours();
-    // Daca ora are o valoare mai mica decat 10 - ii adaugam un 0
     if(hour<10){
         hour = `0${hour}`;
     }
-    // console.log(hour);
 
-    // extragem minutele, daca valoarea e mai mica de 10 ii adaugam un 0
     let minutes = date.getMinutes();
     if(minutes<10){
         minutes=`0${minutes}`;
     }
 
-    // console.log(minutes);
 
-    // Returnam ora sub formatul dorit
     return `${hour}:${minutes}`;
 }
--- utils/weather.js
+++ utils/weather.js
@@ -1,10 +1,8 @@
-// Ne declaram alte functii utilitare pe care o sa le folosim pentru a parsa viteza vantului si iconita
+// Alte functii utilitare folosite pentru a parsa viteza vantului si iconita
 function windToKmPerHour (windPerMeters){
-    // Noi de la API primim viteza vantului in m/s si vrem sa o transformam in km/h
     return (windPerMeters * 3600) / 1000;
 }
 
 function getWeatherIcon (iconCode){
-    // Noi de la API primim un cod pentru iconita si ne folosim de url-ul de la OpenWeather dedicat iconitelor
     return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
 }
