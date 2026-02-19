ad2c04ffcdc797a01d0459f3838cfd1cd54d61ab
Andreea1129 <vilcuandreea12@gmail.com>
2026-02-19 21:07:08 +0200

    remove setCityImage function from this file and update event listeners

--- sections/ChooseLocation.js
+++ sections/ChooseLocation.js
@@ -22,19 +22,13 @@ function updateWeather(cityName) {
   displayWeatherForecast(cityName);
 }
 
-function setCityImage(urlImage, altImage) {
-  const img = document.createElement("img");
-  img.src = urlImage;
-  img.alt = altImage;
-  cityImage.replaceChildren(img);
-}
-
 // Adaug event listenere pe butoane pentru a schimba datele despre vreme si a actualiza imaginea orasului
 aradButton.addEventListener("click", function () {
   updateWeather("Arad");
   setCityImage(
     "https://images.musement.com/cover/0098/62/thumb_9761993_cover_header.jpeg",
     "arad",
+    cityImage,
   );
 });
 
@@ -43,6 +37,7 @@ brasovButton.addEventListener("click", function () {
   setCityImage(
     "https://destinationsmagazine.com/wp-content/uploads/2017/11/Brasov-Town-iStock-501904382-sorincolac.jpg",
     "brasov",
+    cityImage,
   );
 });
 
@@ -51,15 +46,16 @@ bucharestButton.addEventListener("click", function () {
   setCityImage(
     "https://media.gettyimages.com/id/1147589323/photo/palace-of-the-parliament.jpg?s=612x612&w=0&k=20&c=59KgmiBq4wFSD4V3JlmnRiS7n7g19JF58kybaoXeOfQ=",
     "bucuresti",
+    cityImage,
   );
 });
 
 clujButton.addEventListener("click", function () {
   updateWeather("Cluj-Napoca");
-  cityImage.innerHTML = `<img src="https://www.romanianfriend.com/uploads/media/cluj_napoca_1.jpeg" alt='cluj'/>`;
   setCityImage(
     "https://www.romanianfriend.com/uploads/media/cluj_napoca_1.jpeg",
     "cluj",
+    cityImage,
   );
 });
 
@@ -68,6 +64,7 @@ constantaButton.addEventListener("click", function () {
   setCityImage(
     "https://re-value-cities.eu/sites/default/files/media/images/localities/constanta-radub85-dreamstime-122343852jpg.jpg",
     "constanta",
+    cityImage,
   );
 });
 
@@ -76,6 +73,7 @@ iasiButton.addEventListener("click", function () {
   setCityImage(
     "https://www.danagont.ro/wp-content/uploads/2022/08/palatul-culturii-iasi-birds-eye-view.jpg",
     "iasi",
+    cityImage,
   );
 });
 
@@ -84,6 +82,7 @@ oradeaButton.addEventListener("click", function () {
   setCityImage(
     "https://outdooractivities.ro/wp-content/uploads/2019/06/DSC08427.jpg",
     "oradea",
+    cityImage,
   );
 });
 
@@ -92,6 +91,7 @@ sibiuButton.addEventListener("click", function () {
   setCityImage(
     "https://emerging-europe.com/wp-content/uploads/2019/04/dreamstime_m_16982984.jpg",
     "sibiu",
+    cityImage,
   );
 });
 
@@ -100,5 +100,6 @@ timisoaraButton.addEventListener("click", function () {
   setCityImage(
     "https://assets.bucketlistly.blog/sites/5adf778b6eabcc00190b75b1/content_entry5adf77af6eabcc00190b75b6/6451d513d58d15000262f8d3/files/best-things-to-do-in-timisoara-main-image-hd-op.jpg",
     "timisoara",
+    cityImage,
   );
 });
