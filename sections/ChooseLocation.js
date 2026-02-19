fa7e0dfd3a63aa0f29516eb7ec61a68f61d5f816
Andreea1129 <vilcuandreea12@gmail.com>
2026-02-19 17:20:21 +0200

    add setCityImage function and update event listeners

--- sections/ChooseLocation.js
+++ sections/ChooseLocation.js
@@ -1,74 +1,104 @@
-const aradButton = document.querySelector('.dropdown-item.arad');
-const brasovButton = document.querySelector('.dropdown-item.brasov');
-const bucharestButton = document.querySelector('.dropdown-item.bucharest');
-const clujButton = document.querySelector('.dropdown-item.cluj');
-const constantaButton = document.querySelector('.dropdown-item.constanta');
-const iasiButton = document.querySelector('.dropdown-item.iasi');
-const oradeaButton = document.querySelector('.dropdown-item.oradea');
-const sibiuButton = document.querySelector('.dropdown-item.sibiu');
-const timisoaraButton = document.querySelector('.dropdown-item.timisoara');
+const aradButton = document.querySelector(".dropdown-item.arad");
+const brasovButton = document.querySelector(".dropdown-item.brasov");
+const bucharestButton = document.querySelector(".dropdown-item.bucharest");
+const clujButton = document.querySelector(".dropdown-item.cluj");
+const constantaButton = document.querySelector(".dropdown-item.constanta");
+const iasiButton = document.querySelector(".dropdown-item.iasi");
+const oradeaButton = document.querySelector(".dropdown-item.oradea");
+const sibiuButton = document.querySelector(".dropdown-item.sibiu");
+const timisoaraButton = document.querySelector(".dropdown-item.timisoara");
 
 const cityImage = document.querySelector(".city-image");
 
-// Definesc o functie care sa ne schimbe orasul curent afisat pe ecran
-function updateCurrentCityName(city){
-    const currentCity=document.querySelector('.current-city');
-    currentCity.innerHTML= `${city}`;
+function updateCurrentCityName(city) {
+  const currentCity = document.querySelector(".current-city");
+  currentCity.innerHTML = `${city}`;
 }
 
-function updateWeather(cityName){
-    // Salvez in local storage optiunea aleasa
-    localStorage.setItem("city", cityName);
-    // Afisez vremea curenta
-    displayCurrentWeather(cityName);
-    // Apelez functia care ne schimba orasul curent de pe ecran
-    updateCurrentCityName(cityName);
-
-    // Reafisez si prognoza pentru urmatoarele 5 zile
-    displayWeatherForecast(cityName);
+function updateWeather(cityName) {
+  localStorage.setItem("city", cityName);
+  displayCurrentWeather(cityName);
+  updateCurrentCityName(cityName);
+  displayWeatherForecast(cityName);
 }
+
+function setCityImage(urlImage, altImage) {
+  const img = document.createElement("img");
+  img.src = urlImage;
+  img.alt = altImage;
+  cityImage.replaceChildren(img);
+}
+
 // Adaug event listenere pe butoane pentru a schimba datele despre vreme si a actualiza imaginea orasului
-aradButton.addEventListener('click', function(){
-   updateWeather('Arad');
-   cityImage.innerHTML=`<img src="https://images.musement.com/cover/0098/62/thumb_9761993_cover_header.jpeg" alt='arad'/>`;
+aradButton.addEventListener("click", function () {
+  updateWeather("Arad");
+  setCityImage(
+    "https://images.musement.com/cover/0098/62/thumb_9761993_cover_header.jpeg",
+    "arad",
+  );
 });
 
-brasovButton.addEventListener('click', function(){
-   updateWeather('Brașov');
-   cityImage.innerHTML=`<img src="https://destinationsmagazine.com/wp-content/uploads/2017/11/Brasov-Town-iStock-501904382-sorincolac.jpg" alt='brasov'/>`;
+brasovButton.addEventListener("click", function () {
+  updateWeather("Brașov");
+  setCityImage(
+    "https://destinationsmagazine.com/wp-content/uploads/2017/11/Brasov-Town-iStock-501904382-sorincolac.jpg",
+    "brasov",
+  );
 });
 
-bucharestButton.addEventListener('click', function(){
-    updateWeather('București');
-    cityImage.innerHTML=`<img src="https://media.gettyimages.com/id/1147589323/photo/palace-of-the-parliament.jpg?s=612x612&w=0&k=20&c=59KgmiBq4wFSD4V3JlmnRiS7n7g19JF58kybaoXeOfQ=" alt='bucuresti'/>`;
+bucharestButton.addEventListener("click", function () {
+  updateWeather("București");
+  setCityImage(
+    "https://media.gettyimages.com/id/1147589323/photo/palace-of-the-parliament.jpg?s=612x612&w=0&k=20&c=59KgmiBq4wFSD4V3JlmnRiS7n7g19JF58kybaoXeOfQ=",
+    "bucuresti",
+  );
 });
 
-clujButton.addEventListener('click', function(){
-   updateWeather('Cluj-Napoca');
-   cityImage.innerHTML=`<img src="https://www.romanianfriend.com/uploads/media/cluj_napoca_1.jpeg" alt='cluj'/>`;
+clujButton.addEventListener("click", function () {
+  updateWeather("Cluj-Napoca");
+  cityImage.innerHTML = `<img src="https://www.romanianfriend.com/uploads/media/cluj_napoca_1.jpeg" alt='cluj'/>`;
+  setCityImage(
+    "https://www.romanianfriend.com/uploads/media/cluj_napoca_1.jpeg",
+    "cluj",
+  );
 });
 
-constantaButton.addEventListener('click', function(){
-   updateWeather('Constanța');
-   cityImage.innerHTML=`<img src="https://re-value-cities.eu/sites/default/files/media/images/localities/constanta-radub85-dreamstime-122343852jpg.jpg" alt='constanta'/>`;
+constantaButton.addEventListener("click", function () {
+  updateWeather("Constanța");
+  setCityImage(
+    "https://re-value-cities.eu/sites/default/files/media/images/localities/constanta-radub85-dreamstime-122343852jpg.jpg",
+    "constanta",
+  );
 });
 
-iasiButton.addEventListener('click', function(){
-   updateWeather('Iași');
-   cityImage.innerHTML=`<img src="https://www.danagont.ro/wp-content/uploads/2022/08/palatul-culturii-iasi-birds-eye-view.jpg" alt='iasi'/>`;
+iasiButton.addEventListener("click", function () {
+  updateWeather("Iași");
+  setCityImage(
+    "https://www.danagont.ro/wp-content/uploads/2022/08/palatul-culturii-iasi-birds-eye-view.jpg",
+    "iasi",
+  );
 });
 
-oradeaButton.addEventListener('click', function(){
-   updateWeather('Oradea');
-   cityImage.innerHTML=`<img src="https://outdooractivities.ro/wp-content/uploads/2019/06/DSC08427.jpg" alt='oradea'/>`;
+oradeaButton.addEventListener("click", function () {
+  updateWeather("Oradea");
+  setCityImage(
+    "https://outdooractivities.ro/wp-content/uploads/2019/06/DSC08427.jpg",
+    "oradea",
+  );
 });
 
-sibiuButton.addEventListener('click', function(){
-   updateWeather('Sibiu');
-   cityImage.innerHTML=`<img src="https://emerging-europe.com/wp-content/uploads/2019/04/dreamstime_m_16982984.jpg" alt='sibiu'/>`;
+sibiuButton.addEventListener("click", function () {
+  updateWeather("Sibiu");
+  setCityImage(
+    "https://emerging-europe.com/wp-content/uploads/2019/04/dreamstime_m_16982984.jpg",
+    "sibiu",
+  );
 });
 
-timisoaraButton.addEventListener('click', function(){
-    updateWeather('Timișoara');
-    cityImage.innerHTML=`<img src="https://assets.bucketlistly.blog/sites/5adf778b6eabcc00190b75b1/content_entry5adf77af6eabcc00190b75b6/6451d513d58d15000262f8d3/files/best-things-to-do-in-timisoara-main-image-hd-op.jpg" alt='timisoara'/>`;
+timisoaraButton.addEventListener("click", function () {
+  updateWeather("Timișoara");
+  setCityImage(
+    "https://assets.bucketlistly.blog/sites/5adf778b6eabcc00190b75b1/content_entry5adf77af6eabcc00190b75b6/6451d513d58d15000262f8d3/files/best-things-to-do-in-timisoara-main-image-hd-op.jpg",
+    "timisoara",
+  );
 });
